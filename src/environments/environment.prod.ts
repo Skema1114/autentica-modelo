7066a17ee118cc1921832eab77c7d81ef84d644e
Rafael Martins <skema1114@hotmail.com>
2019-11-04 22:11:47 -0300

    Ultimos ajustes, ambiente rodando e app no host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  fireConfig: {
+    apiKey: 'AIzaSyAI3iPz6SLybgjh_ovQeN6_baBkPhoFQGY',
+    authDomain: 'autentica-modelo.firebaseapp.com',
+    databaseURL: 'https://autentica-modelo.firebaseio.com',
+    projectId: 'autentica-modelo',
+    storageBucket: 'autentica-modelo.appspot.com',
+    messagingSenderId: '1033866944376',
+    appId: '1:1033866944376:web:889f7ba49352192b22728d',
+    measurementId: 'G-2RVQEBGYDL'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,14 @@
 export const environment = {
   production: false,
   fireConfig: {
-    apiKey: 'AIzaSyAJRlhL9PdteDQL03-kX5oZuGz7134fg7w',
-    authDomain: 'auth-iffar.firebaseapp.com',
-    databaseURL: 'https://auth-iffar.firebaseio.com',
-    projectId: 'auth-iffar',
-    storageBucket: 'auth-iffar.appspot.com',
-    messagingSenderId: '186757373750',
-    appId: '1:186757373750:web:38295c7beac616120a9d97'
+    apiKey: 'AIzaSyAI3iPz6SLybgjh_ovQeN6_baBkPhoFQGY',
+    authDomain: 'autentica-modelo.firebaseapp.com',
+    databaseURL: 'https://autentica-modelo.firebaseio.com',
+    projectId: 'autentica-modelo',
+    storageBucket: 'autentica-modelo.appspot.com',
+    messagingSenderId: '1033866944376',
+    appId: '1:1033866944376:web:889f7ba49352192b22728d',
+    measurementId: 'G-2RVQEBGYDL'
   }
 };
 
